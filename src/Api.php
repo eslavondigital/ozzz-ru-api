8608614e5827accb2d563abcf13532dd7c2dcfeb
Victor <eslavondigital@gmail.com>
2020-05-21 16:32:43 +0300

    Update Api.php

--- src/Api.php
+++ src/Api.php
@@ -1,57 +1,73 @@
 <?php
-
+/**
+ * Ozzz-ru-api  - Package for working with the Ozzz.ru site API
+ * PHP Version 7.4.
+ *
+ * @see https://github.com/eslavondigital/ozzz-ru-api The Ozzz-ru-api GitHub project
+ *
+ * @author    Vinogradov Victor <eslavon.work.victor@gmail.com>
+ * @author    Eslavon Digital <eslavondigital@gmail.com>
+ * @copyright 2020 Vinogradov Victor
+ * @copyright 2020 Eslavon Digital
+ * @license   https://github.com/eslavondigital/ozzz-ru-api/blob/master/LICENSE MIT License
+ * @note      This program is distributed in the hope that it will be useful - WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.
+ */
 
 namespace Eslavon\Ozzz;
 
 
 use Eslavon\Ozzz\Actions\Balance;
-use Eslavon\Ozzz\Actions\CreateOrder;
+use Eslavon\Ozzz\Actions\Order;
 use Eslavon\Ozzz\Actions\Services;
-use Eslavon\Ozzz\Actions\StatusOrder;
 
+/**
+ * Class Api
+ * @package Eslavon\Ozzz
+ */
 class Api
 {
     /**
-     * API Base host
+     * API Base host.
      */
     protected const API_HOST = 'https://ozzz.ru/api/v2';
 
     /**
-     * Create Order Object
-     * @var CreateOrder $create_order
-     */
-    protected CreateOrder $create_order;
-
-    /**
-     * Status Order Object
-     * @var StatusOrder $status_order
-     */
-    protected StatusOrder $status_order;
-
-    /**
-     * Balance Object
+     * Balance object.
      * @var Balance $balance
      */
     protected Balance $balance;
 
     /**
-     * Services Object
+     * Order object.
+     * @var Order $order
+     */
+    protected Order $order;
+
+    /**
+     * Services object.
      * @var Services $services
      */
     protected Services $services;
 
     /**
-     * Request Object
+     * Request object.
      * @var Request $request
      */
     protected Request $request;
 
     /**
-     * API Key
+     * API Key.
      * @var string $api_key;
      */
     protected string $api_key;
 
+    /**
+     * Api constructor.
+     * @param object $http_client
+     * @param $api_key
+     */
     public function __construct(object $http_client,$api_key)
     {
         $this->request = new Request($http_client, self::API_HOST);
@@ -59,7 +75,7 @@ class Api
     }
 
     /**
-     * Get Object Balance
+     * Get Balance object.
      * @return Balance
      */
     public function balance(): Balance
@@ -71,7 +87,7 @@ class Api
     }
 
     /**
-     * Get Object Services;
+     * Get Services object.
      * @return Services
      */
     public function services(): Services
@@ -83,33 +99,15 @@ class Api
     }
 
     /**
-     * Get Object Status Order
-     * @param int $order_id
-     * @return StatusOrder
+     * Get Order object.
+     * @return Order
      */
-    public function statusOrder(int $order_id): StatusOrder
+    public function order(): Order
     {
-        if (isset($this->status_order) === false) {
-            $this->status_order = new StatusOrder($this->request, $this->api_key, $order_id);
+        if (isset($this->order) === false) {
+            $this->order = new Order($this->request, $this->api_key);
         }
-        return $this->status_order;
+        return $this->order;
     }
-
-    /**
-     * Get Object Create Order
-     * @param int $order_id
-     * @return CreateOrder
-     */
-    public function createOrder(int $id_services, string $link, int $quantity): CreateOrder
-    {
-        if (isset($this->create_order) === false) {
-            $this->create_order = new CreateOrder($this->request, $this->api_key, $id_services, $link, $quantity);
-        }
-        return $this->create_order;
-    }
-
-
-
-
 
 }
