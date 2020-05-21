f211fda5cc37f9c3544b39b91be159f74c2b26fe
Victor <eslavondigital@gmail.com>
2020-05-21 16:33:05 +0300

    Update Balance.php

--- src/Actions/Balance.php
+++ src/Actions/Balance.php
@@ -1,13 +1,32 @@
 <?php
+/**
+ * Ozzz-ru-api  - Package for working with the Ozzz.ru site API
+ * PHP Version 7.4.
+ *
+ * @see https://github.com/eslavondigital/ozzz-ru-api The Ozzz-ru-api GitHub project
+ *
+ * @author    Vinogradov Victor <eslavon.work.victor@gmail.com>
+ * @author    Eslavon Digital <eslavondigital@gmail.com>
+ * @copyright 2020 Vinogradov Victor
+ * @copyright 2020 Eslavon Digital
+ * @license   https://github.com/eslavondigital/ozzz-ru-api/blob/master/LICENSE MIT License
+ * @note      This program is distributed in the hope that it will be useful - WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.
+ */
 
 
 namespace Eslavon\Ozzz\Actions;
 
 
+/**
+ * Class Balance
+ * @package Eslavon\Ozzz\Actions
+ */
 class Balance
 {
     /**
-     * JSON response
+     * Raw body data (JSON).
      * @var string $raw_body_data
      */
     private string $raw_body_data;
@@ -25,7 +44,7 @@ class Balance
     }
 
     /**
-     * Get raw body data
+     * Get raw body data.
      * @return string
      */
     public function getRawBodyData(): string
@@ -34,12 +53,22 @@ class Balance
     }
 
     /**
-     * Get balance
-     * @return string
+     * Get balance.
+     * @return float
      */
-    public function get(): string
+    public function getAmount(): float
     {
         $data = json_decode($this->getRawBodyData());
-        return $data->balance." ".$data->currency;
+        return $data->balance;
+    }
+
+    /**
+     * Get currency.
+     * @return string
+     */
+    public function getCurrency(): string
+    {
+        $data = json_decode($this->getRawBodyData());
+        return $data->currency;
     }
 }
