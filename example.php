d553a20f92e37b6f96ef75521c7b27230cb56082
Victor <eslavondigital@gmail.com>
2020-05-21 16:32:24 +0300

    Update example.php

--- example.php
+++ example.php
@@ -3,21 +3,32 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 use Eslavon\Ozzz\Api;
 
-$api_key = '$2y$10$SzzWhi8Nc0suveMMcGh';
+$api_key = '$2y$10$SzzWhi8Nc0suv';
 $api = new Api(new GuzzleHttp\Client(), $api_key);
 
-// Get Balance
-$balance = $api->balance()->get();
+# Balance sheet data
+# Get raw body data
+$raw_body_data = $api->balance()->getRawBodyData(); // string
+# Get amount balance
+$balance = $api->balance()->getAmount(); // float
+# Get currency
+$currency = $api->balance()->getCurrency(); // string
 
-// Get List Services
-$array_list_services = $api->services()->get();
 
-// Create Order
+# Get a list of services with a description
+# Get raw body data
+$raw_body_data = $api->services()->getRawBodyData(); // string
+# Get the result as an array
+$array_list_services = $api->services()->get(); // array
+
+# Work with orders
+# Create Order
 $id_services = 112;
 $link = "https://t.me/eslavon";
 $quantity = 10;
-$order_id = $api->createOrder($id_services, $link, $quantity);
+$order  = $api->order()->create( $id_services, $link, $quantity); // object
+# Status Order
+$status = $api->order()->status(12707918); // object
+# Cancel Order
+$cancel = $api->order()->cancel(12707918); //object
 
-//Get Status Order
-$order_id = 12689060;
-$status_order = $api->statusOrder($order_id)->get();
